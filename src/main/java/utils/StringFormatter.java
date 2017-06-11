94d12c4043a271171cc1e92d6c386bd0849b35aa
Karol <pokomeda.karol@gmail.com>
2017-06-11 09:39:56 +0200

    Move center method from ListBowGraph, to StringFormatter class

--- src/main/java/utils/StringFormatter.java
+++ src/main/java/utils/StringFormatter.java
@@ -4,4 +4,13 @@ package utils;
  * Created by Karol Pokomeda on 2017-06-11.
  */
 public class StringFormatter {
+    public static String center(String string, int length) {
+        if (length < string.length())
+            throw new IllegalArgumentException("String is longer than the designated placeholder");
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < (length - string.length()) / 2; i++) result.append(" ");
+        result.append(string);
+        while (result.length() < length) result.append(" ");
+        return result.toString();
+    }
 }
