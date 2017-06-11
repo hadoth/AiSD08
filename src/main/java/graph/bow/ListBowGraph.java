19b43aea2f5c9c32f8b1d72f450cb94bef641275
Karol <pokomeda.karol@gmail.com>
2017-06-11 09:40:35 +0200

    Reformat ListBowGraph for external center method

--- src/main/java/graph/bow/ListBowGraph.java
+++ src/main/java/graph/bow/ListBowGraph.java
@@ -1,5 +1,6 @@
 package graph.bow;
 
+import utils.StringFormatter;
 import utils.storage.Vertex;
 
 import java.util.ArrayList;
@@ -154,27 +155,17 @@ public class ListBowGraph<T> implements MyBowGraph<T> {
         }
 
         StringBuilder result = new StringBuilder();
-        result.append(center("", maxLength)).append(" ");
+        result.append(StringFormatter.center("", maxLength)).append(" ");
         for (Vertex<T> vertex : this.vertices) {
-            result.append(" ").append(center(vertex.getValue().toString(), maxLength)).append(" ");
+            result.append(" ").append(StringFormatter.center(vertex.getValue().toString(), maxLength)).append(" ");
         }
         for (Vertex<T> vertex : this.vertices) {
             result.append('\n');
-            result.append(center(vertex.getValue().toString(), maxLength)).append(" ");
+            result.append(StringFormatter.center(vertex.getValue().toString(), maxLength)).append(" ");
             for (Vertex<T> neighbour : this.vertices){
-                result.append("[").append(center(Integer.toString(vertex.getNeighbourWeight(neighbour)), maxLength)).append("]");
+                result.append("[").append(StringFormatter.center(Integer.toString(vertex.getNeighbourWeight(neighbour)), maxLength)).append("]");
             }
         }
-        return result.toString();
-    }
-
-    private static String center(String string, int length) {
-        if (length < string.length())
-            throw new IllegalArgumentException("String is longer than the designated placeholder");
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < (length - string.length()) / 2; i++) result.append(" ");
-        result.append(string);
-        while (result.length() < length) result.append(" ");
         return result.toString();
     }
 }
